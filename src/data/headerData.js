c9982cbb6a2275e93db56a44d3e2dfebcdc7b217
Lancyy <cabanitlance45@gmail.com>
2025-08-22 23:27:32 +0800

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Dane Arrah Candelario',
     title: "Virtual Assistant",
     desciption:"Creativity sparks ideas, but hard work transforms them into reality. Every design, every project, every solution is the result of imagination combined with dedication. When innovation meets consistent effort, possibilities become achievements, and ordinary tasks turn into extraordinary creations. ",
-    image: 'https://raw.githubusercontent.com/Lance-Cabanit/Arrah-Site/refs/heads/master/header.JPG',
+    image: 'https://raw.githubusercontent.com/Lance-Cabanit/Arrah-Site/refs/heads/master/482082882_1185530783150395_9115371664812089771_n.jpg',
     resumePdf: resume
 }
